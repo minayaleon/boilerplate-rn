68fc6a0268f209feb102f9417da22d598d79b9c3
Juan Minaya <minayaleon@gmail.com>
2020-07-29 00:42:04 -0500

    Removed console.logs

--- src/Main.js
+++ src/Main.js
@@ -16,11 +16,8 @@ import AppNavigator from './navigator/AppNavigator';
 import {linking} from "./navigator/linking";
 import {UIProvider} from './app/context/ui';
 import {changeTheme} from './redux/actions/app';
-import {types} from './redux/types';
 
 const Main = (props) => {
-  console.log('Main');
-  console.log(props);
   const checkThemeChange = useRef(false);
   const theme = props.app.theme;
   const [deviceTheme, setDeviceTheme] = useState('');
@@ -37,23 +34,15 @@ const Main = (props) => {
   const txtColor = (theme === defaultTheme) ? 'black' : 'white';
 
   useEffect(() => {
-    console.log('theme observer');
-    console.log(theme, 'app');
-    console.log(deviceTheme, 'device');
-    console.log(props.app.manageTheme, 'manage');
     if (checkThemeChange.current && !props.app.manageTheme && theme !== deviceTheme) {
-      console.log('new value');
       props.dispatch(changeTheme({theme: deviceTheme}));
     }
-    // props.dispatch({type: types.RESET_APP});
   }, [deviceTheme]);
 
   useEffect(() => {
-    console.log('only initial');
     themeChangeListener();
     Appearance.addChangeListener(themeChangeListener);
     return (() => {
-      console.log('remove');
       Appearance.removeChangeListener(themeChangeListener);
     });
   }, []);
