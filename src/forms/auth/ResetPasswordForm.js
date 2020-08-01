266a1737822256dcca6e3f5c5c15cfdd45a60a7e
Juan Minaya <minayaleon@gmail.com>
2020-07-31 21:29:06 -0500

    Match en and es languages

--- src/forms/auth/ResetPasswordForm.js
+++ src/forms/auth/ResetPasswordForm.js
@@ -4,6 +4,7 @@ import * as Yup from 'yup';
 import {View} from 'react-native';
 import {Button, Text, TextInput, HelperText, Paragraph, Dialog, Portal} from 'react-native-paper';
 import {MainHelper} from '@codepso/rn-helper';
+import i18n from 'i18n-js';
 import {formState} from '../state';
 import {appStyle} from '../../assets/styles/app';
 import {formStyle} from '../../assets/styles/form';
@@ -16,39 +17,38 @@ const ResetPasswordForm = props => {
 
   const ResetPasswordSchema = Yup.object().shape({
     password: Yup.string()
-      .required('Please enter a password.')
-      .min(8, 'Password is too short, should be 8 chars minimum.'),
+      .required(i18n.t('app.form.password.rules.required'))
+      .min(8, i18n.t('app.form.password.rules.min')),
     passwordConfirmation: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Passwords must match')
-      .required('Please confirm your password.')
-      .min(8, 'Password is too short, should be 8 chars minimum.')
+      .oneOf([Yup.ref('password'), null], i18n.t('app.form.passwordConfirmation.rules.oneOf'))
+      .required(i18n.t('app.form.passwordConfirmation.rules.required'))
+      .min(8, i18n.t('app.form.passwordConfirmation.rules.min'))
   });
 
   const initialValues = {...formState.resetPassword};
   const dialogTokenExpired = createRef();
   const authService = new AuthService();
 
-  const onSend = async (values) => {
+  const onSend = async (formData) => {
     blockUI.current.open(true);
 
     try {
-      values.token = at;
-      const formData = MainHelper.toSnakeCase(values);
+      formData.token = at;
       await authService.resetPassword(formData);
       blockUI.current.open(false);
       dialogUI.current.open(
-        'Reset Password',
-        'Your passworda has been updated, click OK to continue',
+        i18n.t('resetPassword.txt.dlgTitle'),
+        i18n.t('resetPassword.txt.dlgMessage'),
         {navigation, screen: 'SignIn'},
       );
     } catch (error) {
       blockUI.current.open(false);
       let message = MainHelper.getError(error);
       if (message === 'token_invalid') {
-        message = 'The token has expired, request a new link again to recover your password';
+        message = i18n.t('resetPassword.txt.expiredToken');
         dialogTokenExpired.current.open(true, message);
       } else {
-        dialogUI.current.open('Snap!', message);
+        dialogUI.current.open(i18n.t('app.txt.snap'), message);
       }
     }
   };
@@ -71,7 +71,7 @@ const ResetPasswordForm = props => {
         <Dialog
           visible={open}
           onDismiss={false}>
-          <Dialog.Title>Reset Password</Dialog.Title>
+          <Dialog.Title>{i18n.t('resetPassword.txt.title')}</Dialog.Title>
           <Dialog.Content>
             <Paragraph>{message}</Paragraph>
           </Dialog.Content>
@@ -79,7 +79,7 @@ const ResetPasswordForm = props => {
             <Button onPress={() => {
               setOpen(false);
               navigation.navigate('RecoverPassword');
-            }}>Send new email</Button>
+            }}>{i18n.t('resetPassword.txt.sendNewEmail')}</Button>
           </Dialog.Actions>
         </Dialog>
       </Portal>
@@ -95,14 +95,14 @@ const ResetPasswordForm = props => {
       {(propsForm) => (
         <>
           <DialogTokenExpired ref={dialogTokenExpired} />
-          <Text style={[appStyle.txtTitle, appStyle.setCenter]}>Reset Password</Text>
+          <Text style={[appStyle.txtTitle, appStyle.setCenter]}>{i18n.t('resetPassword.txt.title')}</Text>
           <Text style={[appStyle.setCenter, appStyle.mrg20B]}>
-            Please enter a new password
+            {i18n.t('resetPassword.txt.subtitle')}
           </Text>
           <View style={formStyle.panForm}>
             <TextInput
               mode={'outlined'}
-              placeholder={'Password'}
+              placeholder={i18n.t('app.form.password.label')}
               secureTextEntry={true}
               value={propsForm.values.password}
               onChangeText={propsForm.handleChange('password')}
@@ -115,7 +115,7 @@ const ResetPasswordForm = props => {
             <TextInput
               mode={'outlined'}
               secureTextEntry={true}
-              placeholder={'Confirm Password'}
+              placeholder={i18n.t('app.form.passwordConfirmation.label')}
               value={propsForm.values.passwordConfirmation}
               onChangeText={propsForm.handleChange('passwordConfirmation')}
               onBlur={propsForm.handleBlur('passwordConfirmation')}
@@ -128,7 +128,7 @@ const ResetPasswordForm = props => {
               mode="contained"
               contentStyle={formStyle.btnMain}
               onPress={propsForm.handleSubmit}>
-              Reset
+              {i18n.t('resetPassword.btn.reset')}
             </Button>
           </View>
         </>
