d7785af220333452c3e3bc19b756a57a17d6de26
Juan Minaya <minayaleon@gmail.com>
2021-01-19 21:57:15 -0500

    Added environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   appName: 'Knot',
-  apiMain: 'http://api-local.kunawi.com/v1',
+  apiMain: 'http://api-local.kunawi.com:8701/v1',
   // apiMain: 'http://10.0.2.2:8000/v1',
   deepLink: 'pe.knot://',
   contactAccount: 'support@knot.pe'
